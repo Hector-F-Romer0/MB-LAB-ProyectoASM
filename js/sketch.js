2aaeaa769871bf3d0da21120036e02b23dd97c50
Hector Romero <hector_fabio.romero@uao.edu.co>
2022-10-03 22:41:22 -0500

    Cuaro ajuste de la velociadad. Se añadió un modelo de pruebas adicional en la ranura 3

--- js/sketch.js
+++ js/sketch.js
@@ -2,19 +2,26 @@
 let modelo3DActual;
 let primerModelo3D;
 let segundoModelo3D;
+let tercerModelado3D;
 let alpha = 0;
 let beta;
 let gamma = 0;
 let velocidadRotacion = 0.5;
 let textGamma = document.getElementById("textGama");
+let texturaModeladoActual;
 let texturaPrimerModelado;
+let texturaSegundoModelado;
+let texturaTercerModelado;
 
 // FUNCIÓN DE PRECARGA
 function preload() {
 	// Carga el modelo 3D, normalizado
 	primerModelo3D = loadModel("./sources/models/Primer modelado.obj", true);
 	segundoModelo3D = loadModel("./sources/models/SegundoModelado.obj", true);
-	texturaPrimerModelado = loadImage("./sources/models/Primero/untitled_tongue_albedo.png");
+	tercerModelado3D = loadModel("./sources/models/TercerModelado.obj", true);
+
+	texturaModeladoActual = loadImage("./sources/models/Primero/untitled_body_derm.png");
+	texturaTercerModelado = loadImage("./sources/img/untitled_body_derm3.png");
 	modelo3DActual = primerModelo3D;
 }
 // FUNCIÓN DE CONFIGURACIÓN
@@ -40,7 +47,7 @@ function draw() {
 		rotateY(velocidadRotacion + gamma / 2);
 	}
 	velocidadRotacion += 0.1;
-	texture(texturaPrimerModelado);
+	texture(texturaModeladoActual);
 	// Escala el modelo 3D
 	scale(beta * 0.04);
 	// Rota el modelo 180 grados
@@ -75,11 +82,14 @@ const cambiarModelado = (identificadorModelado) => {
 	switch (identificadorModelado) {
 		case 1:
 			modelo3DActual = primerModelo3D;
+			// texturaModeladoActual = texturaPrimerModelado;
 			break;
 		case 2:
 			modelo3DActual = segundoModelo3D;
 			break;
 		case 3:
+			modelo3DActual = tercerModelado3D;
+			texturaModeladoActual = texturaTercerModelado;
 			break;
 		default:
 			break;
