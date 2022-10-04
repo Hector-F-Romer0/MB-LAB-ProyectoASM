dcdf1fa0952c4b1ebfdf486c9190761a0767b70d
Hector Romero <hector_fabio.romero@uao.edu.co>
2022-10-04 18:42:27 -0500

    Quinta correción de velocidad.

--- js/sketch.js
+++ js/sketch.js
@@ -8,10 +8,6 @@ let beta;
 let gamma = 0;
 let velocidadRotacion = 0.5;
 let textGamma = document.getElementById("textGama");
-let texturaModeladoActual;
-let texturaPrimerModelado;
-let texturaSegundoModelado;
-let texturaTercerModelado;
 
 // FUNCIÓN DE PRECARGA
 function preload() {
@@ -19,9 +15,6 @@ function preload() {
 	primerModelo3D = loadModel("./sources/models/Primer modelado.obj", true);
 	segundoModelo3D = loadModel("./sources/models/SegundoModelado.obj", true);
 	tercerModelado3D = loadModel("./sources/models/TercerModelado.obj", true);
-
-	texturaModeladoActual = loadImage("./sources/models/Primero/untitled_body_derm.png");
-	texturaTercerModelado = loadImage("./sources/img/untitled_body_derm3.png");
 	modelo3DActual = primerModelo3D;
 }
 // FUNCIÓN DE CONFIGURACIÓN
@@ -41,13 +34,13 @@ function draw() {
 
 	// Rota la figura en el eje Y
 	if (gamma > 0) {
-		rotateY(velocidadRotacion + gamma * 1.5);
+		rotateY(velocidadRotacion + gamma * gamma);
 	} else {
 		console.log("Velocidad decreciente", velocidadRotacion * gamma);
-		rotateY(velocidadRotacion + gamma / 2);
+		rotateY((velocidadRotacion + gamma *-gamma);
 	}
 	velocidadRotacion += 0.1;
-	texture(texturaModeladoActual);
+	normalMaterial();
 	// Escala el modelo 3D
 	scale(beta * 0.04);
 	// Rota el modelo 180 grados
@@ -82,14 +75,12 @@ const cambiarModelado = (identificadorModelado) => {
 	switch (identificadorModelado) {
 		case 1:
 			modelo3DActual = primerModelo3D;
-			// texturaModeladoActual = texturaPrimerModelado;
 			break;
 		case 2:
 			modelo3DActual = segundoModelo3D;
 			break;
 		case 3:
 			modelo3DActual = tercerModelado3D;
-			texturaModeladoActual = texturaTercerModelado;
 			break;
 		default:
 			break;
